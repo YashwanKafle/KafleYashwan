f40dbce2f13686b1d745a4167259c9a99c17614a
YashwanKafle <077bct093@ioepc.edu.np>
2024-06-13 11:08:22 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  const linesToType = ["Hello Everyone,", "I'm Yashwan Kafle", "I develop software"]
+
+  const lineElements = [$(".line1"), $(".line2"), $(".line3")]
+
+  let lineIndex = 0
+  let charIndex = 0
+
+  function typeText() {
+    if (lineIndex < linesToType.length) {
+      if (charIndex < linesToType[lineIndex].length) {
+        lineElements[lineIndex].append(linesToType[lineIndex].charAt(charIndex))
+        charIndex++
+        setTimeout(typeText, 100)
+      } else {
+        charIndex = 0
+        lineIndex++
+        setTimeout(typeText, 100)
+      }
+    }
+  }
+
+  typeText()
+})
